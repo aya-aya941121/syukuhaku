64531131ceb93ff2d364e21a34e63cc295ec4b35
aya-aya941121 <94aya1121@gmail.com>
2024-01-20 10:38:48 +0900

    device

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'rooms#index'
+
+  devise_for :users # 追加
+
+  resources :rooms, only: [:index, :show, :new, :create] do
+    collection do
+      get 'search'
+    end
+  end
+
+  resources :reservations, only: [:new, :create]
+  resources :users, only: [:new, :create]
 end
